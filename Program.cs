149d6cd8cb0d8af423f19776ec5ebecd69ac6c15
saravanansubramaniam <you@example.com>
2019-08-02 14:40:57 +0530

    First Commit

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace devopsnetconsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Deploy .NET Core");
+	        Console.ReadKey();
+        }
+    }
+}
